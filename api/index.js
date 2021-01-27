287975bde766e0b36bb56b5e0aa7ec92609442d7
Nathan Chu <63111210+nathanchu@users.noreply.github.com>
2021-01-27 15:53:06 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/plain')
-  res.send((req.headers['x-forwarded-for'] || req.ip || '0.0.0.0') + \n)
+  res.send((req.headers['x-forwarded-for'] || req.ip || '0.0.0.0') + '\n')
 }
